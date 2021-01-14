020fdc186fd1a5f5b753eb942988759965e7b3dd
Damien Miller <djm@mindrot.org>
2021-01-14 15:26:47 +1100

    ssh client side of binding
    send session ID, hostkey, signature and a flag indicating whether the
    agent connection is being forwarded to ssh agent each time a connection
    is opened via a new "session-bind@openssh.com" agent extension.

--- clientloop.c
+++ clientloop.c
@@ -1589,6 +1589,12 @@ client_request_agent(struct ssh *ssh, const char *request_type, int rchan)
 			debug_fr(r, "ssh_get_authentication_socket");
 		return NULL;
 	}
+	if ((r = ssh_agent_bind_hostkey(sock, ssh->kex->initial_hostkey,
+	    ssh->kex->session_id, ssh->kex->initial_sig, 1)) == 0)
+		debug_f("bound agent to hostkey");
+	else
+		debug2_fr(r, "ssh_agent_bind_hostkey");
+
 	c = channel_new(ssh, "authentication agent connection",
 	    SSH_CHANNEL_OPEN, sock, sock, -1,
 	    CHAN_X11_WINDOW_DEFAULT, CHAN_TCP_PACKET_DEFAULT, 0,
