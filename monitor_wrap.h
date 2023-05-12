25a2c35de9989c3b3561e1cce79843832646164a
Damien Miller <djm@mindrot.org>
2023-05-12 13:46:53 +1000

    remove SRCS_PROT from listener
    a couple of functions in servconf.c called into channels.c, move them
    to monitor_wrap.c

--- monitor_wrap.h
+++ monitor_wrap.h
@@ -83,4 +83,8 @@ void mm_send_keystate(struct ssh *, struct monitor*);
 int mm_bsdauth_query(void *, char **, char **, u_int *, char ***, u_int **);
 int mm_bsdauth_respond(void *, u_int, char **);
 
+/* config / channels glue */
+void	 server_process_permitopen(struct ssh *);
+void	 server_process_channel_timeouts(struct ssh *ssh);
+
 #endif /* _MM_WRAP_H_ */
