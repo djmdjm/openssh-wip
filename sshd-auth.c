80fb908dd49a4718fbb720cdb604689a21bfe5ce
Markus Friedl <mfriedl@gmail.com>
2025-04-02 15:23:06 +0200

    implement mlkem768nistp256-sha256 based on kexmlkem768x25519.c

--- sshd-auth.c
+++ sshd-auth.c
@@ -765,6 +765,7 @@ do_ssh2_kex(struct ssh *ssh)
 	kex->kex[KEX_DH_GEX_SHA1] = kexgex_server;
 	kex->kex[KEX_DH_GEX_SHA256] = kexgex_server;
 	kex->kex[KEX_ECDH_SHA2] = kex_gen_server;
+	kex->kex[KEX_KEM_MLKEM768ECDH_SHA256] = kex_gen_server;
 #endif
 	kex->kex[KEX_C25519_SHA256] = kex_gen_server;
 	kex->kex[KEX_KEM_SNTRUP761X25519_SHA512] = kex_gen_server;
